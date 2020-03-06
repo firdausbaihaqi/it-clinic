11f26fd1c4d9a4f43826694a37f6d510c1964a5f
Andry Dwi Suharmanto <andry.dwi.s@gmail.com>
2020-03-06 19:28:42 +0700

    Add files via upload

--- project.sql
+++ project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 06, 2020 at 10:47 AM
+-- Generation Time: Mar 06, 2020 at 01:27 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -59,7 +59,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `user`, `password`, `status`) VALUES
-(1, 'admin', 'admin', 'customer-unverified');
+(1, 'admin', 'admin', 'customer-unverified'),
+(3, 'andry', '123', 'technician-unverified'),
+(15, 'anjay', '123', 'technician-unverified'),
+(20, 'a', 'a', 'customer-unverified');
 
 -- --------------------------------------------------------
 
@@ -81,7 +84,34 @@ CREATE TABLE `user_customer` (
 --
 
 INSERT INTO `user_customer` (`id`, `user`, `password`, `email`, `address`, `phone`) VALUES
-(1, 'admin', 'admin', 'andry.dwi.s@gmail.coma', 'malang', '08986895328');
+(1, 'admin', 'admin', 'andry.dwi.s@gmail.coma', 'malang', '08986895328'),
+(2, 'a', 'a', 'andry.knights@gmail.com', 'a', 'a');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_technician`
+--
+
+CREATE TABLE `user_technician` (
+  `id` int(11) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `education` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_technician`
+--
+
+INSERT INTO `user_technician` (`id`, `user`, `password`, `fullname`, `email`, `address`, `phone`, `education`) VALUES
+(1, 'andry', '123', 'Andry Dwi S', 'andry.dwi.s@gmail.com', 'malang', '08986895328', 'D3'),
+(6, 'admin', 'a', 'Andry Dwi S', 'andry.dwi.s@gmail.com', 'a', 'a', 'SMA/SMK'),
+(7, 'anjay', '123', '1', 'andry.dwi.s@gmail.com', 'malang', '1234', 'D1/D2');
 
 --
 -- Indexes for dumped tables
@@ -108,6 +138,13 @@ ALTER TABLE `user_customer`
   ADD UNIQUE KEY `user` (`user`);
 
 --
+-- Indexes for table `user_technician`
+--
+ALTER TABLE `user_technician`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user` (`user`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -121,13 +158,35 @@ ALTER TABLE `main`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `user_customer`
 --
 ALTER TABLE `user_customer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user_technician`
+--
+ALTER TABLE `user_technician`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user_customer`
+--
+ALTER TABLE `user_customer`
+  ADD CONSTRAINT `user_customer_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user_technician`
+--
+ALTER TABLE `user_technician`
+  ADD CONSTRAINT `user_technician_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`user`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
