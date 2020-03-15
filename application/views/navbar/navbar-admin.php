d9902ad9c285929ed991fe4a02144258d18db6c1
effendikiki <raito12cs@gmail.com>
2020-03-15 12:29:00 +0700

    fixing admin fronted

--- application/views/navbar/navbar-admin.php
+++ application/views/navbar/navbar-admin.php
@@ -7,11 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo $title; ?></title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css">
-    <link rel="stylesheet" href="D:\xampp\htdocs\it-clinic\assets\css\theme.css">
-    <link rel="stylesheet" href="D:\xampp\htdocs\it-clinic\assets\css\demo.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/theme.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/demo.css">
 
 </head>
 
@@ -50,20 +49,17 @@
                                     <h5 class="heading h6 mb-0">User Pending List</h5>
                                 </div>
                                 <div class="list-group">
-                                    <a href="#"
-                                        class="list-group-item list-group-item-action d-flex align-items-center">
+                                    <a href="#" class="list-group-item list-group-item-action d-flex align-items-center">
                                         <div class="list-group-content">
                                             <div class="list-group-heading">andry sadboy</div>
                                         </div>
                                     </a>
-                                    <a href="#"
-                                        class="list-group-item list-group-item-action d-flex align-items-center">
+                                    <a href="#" class="list-group-item list-group-item-action d-flex align-items-center">
                                         <div class="list-group-content">
                                             <div class="list-group-heading">rizal bucin</div>
                                         </div>
                                     </a>
-                                    <a href="#"
-                                        class="list-group-item list-group-item-action d-flex align-items-center">
+                                    <a href="#" class="list-group-item list-group-item-action d-flex align-items-center">
                                         <div class="list-group-content">
                                             <div class="list-group-heading">lord ihya</div>
                                         </div>
@@ -85,12 +81,8 @@
     <!-- lek wes mari dihapus ae -->
 
     <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </body>
 
