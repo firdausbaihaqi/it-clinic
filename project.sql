4a0678f71d611715f12fae81be160924b5fdbbc1
Andry Dwi Suharmanto <andry.dwi.s@gmail.com>
2020-03-22 12:04:05 +0700

    update this first

--- project.sql
+++ project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 14, 2020 at 09:47 AM
+-- Generation Time: Mar 22, 2020 at 06:01 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -30,6 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `order` (
   `id` int(11) NOT NULL,
+  `code_order` int(11) NOT NULL,
   `customer` varchar(255) NOT NULL,
   `technician` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
@@ -41,6 +42,16 @@ CREATE TABLE `order` (
   `shipment` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `order`
+--
+
+INSERT INTO `order` (`id`, `code_order`, `customer`, `technician`, `image`, `detail`, `status`, `date_order`, `date_finish`, `price`, `shipment`) VALUES
+(3, 1677978096, 'andry', 'default', 'andry.png', 'error mas,ngelu aku', 'in_progress', '2020-03-21', '0000-00-00', 0, ''),
+(11, 26335840, 'andry', 'default', 'andry.jpg', 'aaaaaaaaaaaaa', 'in_queue', '2020-03-22', '0000-00-00', 0, ''),
+(12, 95869841, 'andry', 'default', 'andry1.png', 'wes kenek mas', 'in_queue', '2020-03-22', '0000-00-00', 0, ''),
+(13, 1200570903, 'andry', 'default', 'andry2.png', 'erer', 'in_queue', '2020-03-22', '0000-00-00', 0, '');
+
 -- --------------------------------------------------------
 
 --
@@ -51,6 +62,7 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `user` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -58,12 +70,14 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `user`, `password`, `status`) VALUES
-(1, 'admin', 'admin', 'admin'),
-(2, 'andry', '123', 'technician'),
-(5, 'andryyy', '123', 'technician'),
-(7, 'kiki', '`23', 'technician-unverified'),
-(8, 'andrydwis', '123', 'customer-unverified');
+INSERT INTO `user` (`id`, `user`, `password`, `image`, `status`) VALUES
+(1, 'admin', 'admin', '', 'admin'),
+(2, 'default', 'default', '', 'technician'),
+(3, 'andry', '123', 'andry.jpg', 'customer'),
+(4, 'kiki', '123', 'kiki.png', 'technician-unverified'),
+(5, 'rijalll', '123', 'rijalll.jpg', 'technician'),
+(6, 'lemon', '123', 'lemon.jpg', 'technician-unverified'),
+(7, 'bukan', '123', 'bukan.jpg', 'customer-unverified');
 
 -- --------------------------------------------------------
 
@@ -87,7 +101,8 @@ CREATE TABLE `user_customer` (
 --
 
 INSERT INTO `user_customer` (`id`, `user`, `password`, `fullname`, `image`, `email`, `address`, `phone`) VALUES
-(18, 'andrydwis', '123', 'Andry Dwi S', 'andrydwis.jpg', 'andry.dwi.s@gmail.com', 'malang', '08986895328');
+(1, 'andry', '123', 'Andry Dwi S', 'andry.jpg', 'andry.dwi.s@gmail.com', 'malang', '08986895328'),
+(2, 'bukan', '123', 'Andry Dwi S', 'bukan.jpg', 'andry.dwi.s@gmail.com', 'malang', '08986895328');
 
 -- --------------------------------------------------------
 
@@ -112,9 +127,10 @@ CREATE TABLE `user_technician` (
 --
 
 INSERT INTO `user_technician` (`id`, `user`, `password`, `fullname`, `image`, `email`, `address`, `phone`, `education`) VALUES
-(38, 'andry', '123', 'Andry Dwi S', 'andry.jpg', 'andry.dwi.s@gmail.com', 'malang', '08986895328', 'D3'),
-(39, 'andryyy', '123', 'Andry Dwi S', 'andryyy.jpg', 'andry.dwi.s@gmail.com', 'Semanggi Barat no 25', '08986895328', 'D1/D2'),
-(41, 'kiki', '`23', 'fiqiiiiiii', 'kiki1.jpg', 'andry.dwi.s@gmail.com', '11', '1', 'SMA/SMK');
+(1, 'default', 'default', '', '', '', '', '', ''),
+(2, 'kiki', '123', 'kiki wibu', 'kiki.png', 'andry.dwi.s@gmail.com', 'malang', '08986895328', 'S1'),
+(3, 'rijalll', '123', 'rizal', 'rijalll.jpg', 'andry.dwi.s@gmail.com', 'malang', '08986895328', 'SMA/SMK'),
+(4, 'lemon', '123', 'lemon', 'lemon.jpg', 'andry.knights@gmail.com', 'malang', '123', 'SMA/SMK');
 
 --
 -- Indexes for dumped tables
@@ -154,22 +170,28 @@ ALTER TABLE `user_technician`
 --
 
 --
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `user_customer`
 --
 ALTER TABLE `user_customer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user_technician`
 --
 ALTER TABLE `user_technician`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
